71f5f9241572625c597ab94a41a199d43f672411
Rafael Coelho <rafaellcoellho@gmail.com>
2019-03-08 20:10:32 -0300

    .

--- src/game.c
+++ src/game.c
@@ -21,138 +21,138 @@ int game_speed = 20;
 
 static void init_sdl(void) 
 {
-	int rendererFlags, windowFlags;
-	rendererFlags = SDL_RENDERER_ACCELERATED;
-	windowFlags = 0;
+    int rendererFlags, windowFlags;
+    rendererFlags = SDL_RENDERER_ACCELERATED;
+    windowFlags = 0;
 
-	if(SDL_Init(SDL_INIT_VIDEO) < 0) {
-		printf("Couldn't initialize SDL: %s\n", SDL_GetError());
-		exit(1);
-	}
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("Couldn't initialize SDL: %s\n", SDL_GetError());
+        exit(1);
+    }
 
-	window = SDL_CreateWindow("Tetris", 
-		SDL_WINDOWPOS_UNDEFINED, 
-		SDL_WINDOWPOS_UNDEFINED, 
-		SCREEN_WIDTH, 
-		SCREEN_HEIGHT, 
-		windowFlags
-	);
+    window = SDL_CreateWindow("Tetris", 
+        SDL_WINDOWPOS_UNDEFINED, 
+        SDL_WINDOWPOS_UNDEFINED, 
+        SCREEN_WIDTH, 
+        SCREEN_HEIGHT, 
+        windowFlags
+    );
 
-	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-	renderer = SDL_CreateRenderer(window, -1, rendererFlags);
-	IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    renderer = SDL_CreateRenderer(window, -1, rendererFlags);
+    IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
 }
 
 static void cap_frame_rate(long *then, float *remainder)
 {
-	long wait, frameTime;
-	
-	wait = 16 + *remainder;
-	*remainder -= (int)*remainder;
-	
-	frameTime = SDL_GetTicks() - *then;
-	wait -= frameTime;
-	
-	if (wait < 1) 
-		wait = 1;
-		
-	SDL_Delay(wait);
-	
-	*remainder += 0.667;
-	*then = SDL_GetTicks();
+    long wait, frameTime;
+    
+    wait = 16 + *remainder;
+    *remainder -= (int)*remainder;
+    
+    frameTime = SDL_GetTicks() - *then;
+    wait -= frameTime;
+    
+    if (wait < 1) 
+        wait = 1;
+        
+    SDL_Delay(wait);
+    
+    *remainder += 0.667;
+    *then = SDL_GetTicks();
 }
 
 static void prepare_scene(void)
 {
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
 }
 
 static void present_scene(void)
 {
-	SDL_RenderPresent(renderer);
+    SDL_RenderPresent(renderer);
 }
 
 static void move_tetromino(void)
 {
-	static int filter = 5;
-	static bool space_hold = false;
+    static int filter = 5;
+    static bool space_hold = false;
 
-	if(filter == 0) {
-		if (left_arrow_key()) {
-			if(Tetromino_Fit(p_board, current_tetromino, current_rotation, current_x-1, current_y)) current_x -= 1;
-		}
+    if(filter == 0) {
+        if (left_arrow_key()) {
+            if(Tetromino_Fit(p_board, current_tetromino, current_rotation, current_x-1, current_y)) current_x -= 1;
+        }
 
-		if (right_arrow_key()) {
-			if(Tetromino_Fit(p_board, current_tetromino, current_rotation, current_x+1, current_y)) current_x += 1;
-		}
+        if (right_arrow_key()) {
+            if(Tetromino_Fit(p_board, current_tetromino, current_rotation, current_x+1, current_y)) current_x += 1;
+        }
 
-		if (down_arrow_key()) {
-			if(Tetromino_Fit(p_board, current_tetromino, current_rotation, current_x, current_y+1)) current_y += 1;
-		}
+        if (down_arrow_key()) {
+            if(Tetromino_Fit(p_board, current_tetromino, current_rotation, current_x, current_y+1)) current_y += 1;
+        }
 
-		if (space_key()) {
-			if(!space_hold && Tetromino_Fit(p_board, current_tetromino, current_rotation+1, current_x, current_y)) current_rotation += 1;
-			space_hold = true;
-		} else space_hold = false;
+        if (space_key()) {
+            if(!space_hold && Tetromino_Fit(p_board, current_tetromino, current_rotation+1, current_x, current_y)) current_rotation += 1;
+            space_hold = true;
+        } else space_hold = false;
 
-		filter = 5;
-	}
-	filter--;
+        filter = 5;
+    }
+    filter--;
 }
 
 static void force_down(void)
 {
-	static int speed_counter = 0;
+    static int speed_counter = 0;
 
-	speed_counter++;
-	can_force_down = (speed_counter == game_speed);
+    speed_counter++;
+    can_force_down = (speed_counter == game_speed);
 
-	if(can_force_down) {
-		if(Tetromino_Fit(p_board, current_tetromino, current_rotation, current_x, current_y+1)) current_y++;
-		else {
+    if(can_force_down) {
+        if(Tetromino_Fit(p_board, current_tetromino, current_rotation, current_x, current_y+1)) current_y++;
+        else {
 
-			Tetromuni_Lock(p_board, current_tetromino, current_rotation, current_x, current_y);
+            Tetromuni_Lock(p_board, current_tetromino, current_rotation, current_x, current_y);
 
-			for(int py = 0; py < 4; py++) {
-				if(current_y + py < BOARD_HEIGHT - 1) {
-					bool line = true;
-					int index_board;
-					for(int px = 1; px < BOARD_WIDTH-1; px++) {
-						index_board = (current_y + py) * BOARD_WIDTH + px;
-						line &= (p_board[index_board]) != 0;
-					}
-					if(line) {
-						for(int px = 1; px < BOARD_WIDTH-1; px++) {
-							for(int aux = current_y+py; aux > 0; aux--) {
-								p_board[aux * BOARD_WIDTH + px] = p_board[(aux - 1) * BOARD_WIDTH + px];
-							}
-						}
-					}
-				}
-			}
+            for(int py = 0; py < 4; py++) {
+                if(current_y + py < BOARD_HEIGHT - 1) {
+                    bool line = true;
+                    int index_board;
+                    for(int px = 1; px < BOARD_WIDTH-1; px++) {
+                        index_board = (current_y + py) * BOARD_WIDTH + px;
+                        line &= (p_board[index_board]) != 0;
+                    }
+                    if(line) {
+                        for(int px = 1; px < BOARD_WIDTH-1; px++) {
+                            for(int aux = current_y+py; aux > 0; aux--) {
+                                p_board[aux * BOARD_WIDTH + px] = p_board[(aux - 1) * BOARD_WIDTH + px];
+                            }
+                        }
+                    }
+                }
+            }
 
-			current_x = BOARD_WIDTH / 2;
-			current_y = 0;
-			current_rotation = 0;
-			current_tetromino = Tetromino_Draws();
+            current_x = BOARD_WIDTH / 2;
+            current_y = 0;
+            current_rotation = 0;
+            current_tetromino = Tetromino_Draws();
 
-			game_over = !Tetromino_Fit(p_board, current_tetromino, current_rotation, current_x, current_y);
-		}
-		speed_counter = 0;
-	}
+            game_over = !Tetromino_Fit(p_board, current_tetromino, current_rotation, current_x, current_y);
+        }
+        speed_counter = 0;
+    }
 }
 
 static void logic(void)
 {
-	move_tetromino();
-	force_down();
+    move_tetromino();
+    force_down();
 }
 
 void Game_Create(void)
 {
-	init_sdl();
-	p_board = Board_Create();
+    init_sdl();
+    p_board = Board_Create();
 }
 
 void Game_Loop(void)
--- src/input.c
+++ src/input.c
@@ -5,58 +5,58 @@ static int keyboard[MAX_KEYBOARD_KEYS];
 
 void key_up(SDL_KeyboardEvent *event)
 {
-	if (event->repeat == 0 && event->keysym.scancode < MAX_KEYBOARD_KEYS)
-		keyboard[event->keysym.scancode] = 0;
+    if (event->repeat == 0 && event->keysym.scancode < MAX_KEYBOARD_KEYS)
+        keyboard[event->keysym.scancode] = 0;
 }
 
 void key_down(SDL_KeyboardEvent *event)
 {
-	if (event->repeat == 0 && event->keysym.scancode < MAX_KEYBOARD_KEYS)
-		keyboard[event->keysym.scancode] = 1;
+    if (event->repeat == 0 && event->keysym.scancode < MAX_KEYBOARD_KEYS)
+        keyboard[event->keysym.scancode] = 1;
 }
 
 void read_input(bool *exit_game)
 {
-	SDL_Event event;
-	
-	while(SDL_PollEvent(&event)) {
-		switch(event.type) {
-			case SDL_QUIT:
-				*exit_game = true;
-				break;
-			case SDL_KEYDOWN:
-				key_down(&event.key);
-				break;
-			case SDL_KEYUP:
-				key_up(&event.key);
-				break;
-			default:
-				break;
-		}
-	}
+    SDL_Event event;
+    
+    while(SDL_PollEvent(&event)) {
+        switch(event.type) {
+            case SDL_QUIT:
+                *exit_game = true;
+                break;
+            case SDL_KEYDOWN:
+                key_down(&event.key);
+                break;
+            case SDL_KEYUP:
+                key_up(&event.key);
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 bool left_arrow_key(void) 
 {
-	return keyboard[SDL_SCANCODE_LEFT];
+    return keyboard[SDL_SCANCODE_LEFT];
 }
 
 bool right_arrow_key(void) 
 {
-	return keyboard[SDL_SCANCODE_RIGHT];
+    return keyboard[SDL_SCANCODE_RIGHT];
 }
 
 bool down_arrow_key(void) 
 {
-	return keyboard[SDL_SCANCODE_DOWN];
+    return keyboard[SDL_SCANCODE_DOWN];
 }
 
 bool space_key(void) 
 {
-	return keyboard[SDL_SCANCODE_SPACE];
+    return keyboard[SDL_SCANCODE_SPACE];
 }
 
 bool esc_key(void)
 {
-	return keyboard[SDL_SCANCODE_ESCAPE];
+    return keyboard[SDL_SCANCODE_ESCAPE];
 }
